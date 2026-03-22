a944d8edd78021894672c12ec672d612d8a11198
gustavomaza-blip <gustavomaza-blip@users.noreply.github.com>
2026-03-22 15:53:12 -0500

    feat: integracion de calidad del aire y clima con macros Jinja

---
+++ dbt/proyecto_unl/macros/clasificacion_aire.sql
@@ -0,0 +1,7 @@
+﻿{% macro clasificar_calidad_aire(indice_ica) %}
+    CASE
+        WHEN {{ indice_ica }} <= 50 THEN 'Bueno'
+        WHEN {{ indice_ica }} <= 100 THEN 'Moderado'
+        ELSE 'Dañino'
+    END
+{% endmacro %}
---
+++ dbt/proyecto_unl/models/staging/stg_calidad_aire.sql
@@ -0,0 +1,5 @@
+SELECT 
+    ciudad_id AS nombre_ciudad,
+    indice_ica,
+    particulas_pm25
+FROM calidad_aire
